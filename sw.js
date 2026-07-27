d74154a5e2aa20d99664806a7a5b67dde5e5441a
Muong Maraing <muongmaraing@gmail.com>
2026-07-27 19:54:48 +0700

    Initial commit - Khmer Lunar Calendar website

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'khmer-lunar-v1';
+const FILES = [
+  './',
+  './index.html',
+  './khmer-lunar-calendar.js',
+  './manifest.json',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
